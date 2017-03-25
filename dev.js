dcd812e0c9e12a34eddc7dd81d6e1655225cf5e2
Rainfore <rainforest92@126.com>
2017-03-25 19:42:39 +0800

    :tada: Initial commit
    Signed-off-by: Rainfore <rainforest92@126.com>

---
+++ dev.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['./index.js'],
+    rules: {
+        // Possible Errors
+        'no-console': [0],
+        'no-debugger': [0],
+        'no-empty': [1, { allowEmptyCatch: true }],
+        'no-unreachable': [1],
+
+        // Variables
+        'no-unused-vars': [0],
+    },
+};
