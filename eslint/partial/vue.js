3401a144bfaba8c53565f1654a95199c1022c6fa
Rainfore <rainforest92@126.com>
2019-01-11 15:28:43 +0800

    :arrow_up: Upgrade eslint@5.12.0, babel-eslint@10.0.1, eslint-plugin-vue@5.1.0, vue-eslint-parser@5.0.0

--- eslint/partial/vue.js
+++ eslint/partial/vue.js
@@ -3,47 +3,64 @@ module.exports = {
     parser: 'vue-eslint-parser',
     parserOptions: {
         parser: 'babel-eslint',
-        ecmaVersion: 2017,
         sourceType: 'module',
-        ecmaFeatures: {
-            experimentalObjectRestSpread: true,
-        },
+        ecmaVersion: 2018,
     },
     plugins: ['vue'],
     rules: {
-        'vue/html-indent': [0, 4, {
-            attribute: 1,
-            closeBracket: 0,
-        }], // @has-problem
-        'vue/script-indent': [0, 4, {
-            switchCase: 1,
-        }], // @has-problem
+        // Priority A: Essential
         'vue/no-unused-vars': [1],
         'vue/require-v-for-key': [1],
         'vue/no-use-v-if-with-v-for': [1],
         'vue/no-confusing-v-for-v-if': [0],
-        'vue/name-property-casing': [2, 'kebab-case'],
+
+        // Priority B: Strongly Recommended
+        'vue/html-closing-bracket-newline': [2],
+        'vue/html-closing-bracket-spacing': [2],
+        'vue/html-indent': [2, 4, {
+            baseIndent: 0,
+            // attribute: 1,
+            // closeBracket: 0,
+        }],
         'vue/html-self-closing': [2, {
             html: { void: 'never', normal: 'never', component: 'never' },
             svg: 'always',
             math: 'always',
         }],
         'vue/max-attributes-per-line': [0],
+        'vue/no-spaces-around-equal-signs-in-attribute': [2],
+        'vue/multiline-html-element-content-newline': [0],
+        'vue/name-property-casing': [2, 'kebab-case'],
+        'vue/singleline-html-element-content-newline': [0],
         'vue/require-default-prop': [0],
+
+        // Priority C: Recommended
         'vue/order-in-components': [1],
-        'vue/html-closing-bracket-newline': [2],
-        'vue/html-closing-bracket-spacing': [2],
-        'vue/attributes-order': [0, { order: [
-            'DEFINITION',
-            'LIST_RENDERING',
-            'CONDITIONALS',
-            'RENDER_MODIFIERS',
-            'GLOBAL',
-            'UNIQUE',
-            'BINDING',
-            'OTHER_ATTR',
-            'EVENTS',
-            'CONTENT',
-        ] }],
+        'vue/attributes-order': [2], // Default order is good
+
+        // Uncategorized
+        'vue/array-bracket-spacing': [2, 'never'],
+        'vue/component-name-in-template-casing': [2, 'kebab-case'],
+        'vue/eqeqeq': [2, 'always'],
+        'vue/key-spacing': [2],
+        'vue/match-component-file-name': [2, {
+            extensions: ['jsx', 'vue'],
+            shouldMatchCase: true,
+        }],
+        // @TODO: 'vue/no-restricted-syntax':
+        'vue/object-curly-spacing': [2, 'always'],
+        // @disagree: vue/require-direct-export
+        'vue/script-indent': [2, 4, {
+            baseIndent: 0,
+            switchCase: 1,
+        }],
+        'vue/space-infix-ops': [2],
+        'vue/space-unary-ops': [2],
     },
+    overrides: [{
+        'files': ['*.vue'],
+        'rules': {
+            'indent': 'off',
+        },
+    }],
 };
